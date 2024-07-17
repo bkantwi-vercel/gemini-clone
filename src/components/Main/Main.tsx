62af867895f054b144d00501a4c9459ffa0a92e6
bkantwi <bernardantwi2010@gmail.com>
2024-07-17 13:37:34 +0000

    closed on integration

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -14,28 +14,52 @@ const Main = () => {
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello, User.</span></p>
-                    <p>How may I be of assistance?</p>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>Suggest Beautiful places to see on an upcoming trip</p>
-                        <img src={assets.compass_icon} alt="" />
+
+                {!showResult
+                    ? <>
+                        <div className="greet">
+                            <p><span>Hello, User.</span></p>
+                            <p>How may I be of assistance?</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card">
+                                <p>Suggest Beautiful places to see on an upcoming trip</p>
+                                <img src={assets.compass_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Briefly explain what quantum computing is</p>
+                                <img src={assets.bulb_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Improve the readability of the following code</p>
+                                <img src={assets.code_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Suggest great captions for my Linkedin and Instagram posts</p>
+                                <img src={assets.message_icon} alt="" />
+                            </div>
+                        </div>
+                    </>
+                    :
+                    <div className='result'>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt="" />
+                            {loading
+                                ? <div className='loader'>
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                                :
+                                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                            }
+                        </div>
                     </div>
-                    <div className="card">
-                        <p>Briefly explain what quantum computing is</p>
-                        <img src={assets.bulb_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Improve the readability of the following code</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Suggest great captions for my Linkedin and Instagram posts</p>
-                        <img src={assets.message_icon} alt="" />
-                    </div>
-                </div>
+                }
 
                 <div className="main-bottom">
                     <div className="search-box">
